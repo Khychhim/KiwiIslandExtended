74dabb3a30ecdd5d669205a5fb9347ac6e258c0d
joshl <joshl@192.168.1.12>
2017-04-07 01:08:52 +1200

    Successfully generates a usable map

--- src/nz/ac/aut/ense701/gameModel/DifferentMap.java
+++ src/nz/ac/aut/ense701/gameModel/DifferentMap.java
@@ -15,6 +15,8 @@ public class DifferentMap {
     private static final String TEXT_FORMAT = "UTF-8";
     private final int mapRows;
     private final int mapCols;
+    private int playerRow;
+    private int playerCol;
     private final String playerName;
     private final MapDataTypes mapDataTypes;
     
@@ -32,6 +34,15 @@ public class DifferentMap {
     private static final int RAND_TOOLS = 3; //Up to an addional X of each tool
     private static final int RAND_FAUNA = 3;
     private static final int RAND_HAZARDS = 4;
+    //Minimum distances between spawns
+    private static final int PRED_MIN_DIST = 3; //Min Distance between predator and kiwi
+    private static final int HAZARD_MIN_DIST = 1; //Min Distance between other hazards
+    
+    //For testing map Generation
+    public static void main(String args[]) {
+        DifferentMap test = new DifferentMap(20, 20, "Cheese");
+        test.generateMap();
+    }     
     
     public DifferentMap(int mapRows, int mapCols, String playerName) {
         this.mapRows = mapRows;
@@ -45,7 +56,7 @@ public class DifferentMap {
         PrintWriter pw = CreateFile();
         Random rand = new Random();
         //Setup Maps rows and columns
-        pw.append(mapRows + ", " + mapCols + ",");
+        pw.println(mapRows + ", " + mapCols + ",");
         //Setup the maps Tiles
         for(int r = 0; r < mapRows; r++) {
             String row = "";
@@ -54,13 +65,13 @@ public class DifferentMap {
                 row += mapDataTypes.tileTypes.get(tileToAdd);
             }
             row += ",";
-            pw.append(row);
+            pw.println(row);
         }
         //Setup player stats
-        int playerStartRow = rand.nextInt(mapRows);
-        int playerStartCol = rand.nextInt(mapCols);
+        playerRow = rand.nextInt(mapRows);
+        playerCol = rand.nextInt(mapCols);
         //Sets up the player with 100 max stamina, 10 max backpack weight and 5 max backpack size
-        pw.append(playerName + ", " + playerStartRow + ", " + playerStartCol + ", "
+        pw.println(playerName + ", " + playerRow + ", " + playerCol + ", "
                 + " 100.0, 10.0, 5.0,");
         //Sets up the number of each occupant and the total occupants
         int totalOccupants = 0;
@@ -75,7 +86,163 @@ public class DifferentMap {
         int numberFauna = rand.nextInt(RAND_FAUNA) + MIN_FAUNA + 1;
         int numberHazards = rand.nextInt(RAND_HAZARDS) + MIN_HAZARDS + 1;
         totalOccupants += numberKiwis + numberPredators + numberFood + numberFauna + numberHazards;
-        pw.append(totalOccupants + ",");
+        pw.println(totalOccupants + ",");
+        //Create a temporary map representation to track occupant positions and counts for viability
+        String[][] tempMap = new String[mapRows][mapCols];
+        //Generate the occupants any predators that cannot be generated will be replaced with extra food
+        generateTools(pw, numberTools, tempMap);
+        generateFauna(pw, numberFauna, tempMap);
+        generateHazards(pw, numberHazards, tempMap);
+        generateKiwis(pw, numberKiwis, tempMap);
+        int failedPredGens = generatePredators(pw, numberPredators, tempMap);
+        generateFood(pw, numberFood+failedPredGens, tempMap);
+        //Close the stream after generation is finished
+        pw.close();
+    }
+    
+    private void generateFood(PrintWriter pw, int numberOfFood, String[][] tempMap) {
+        int generatedCount = 0;
+        Random rand = new Random();
+        while(generatedCount < numberOfFood) {
+            int row = rand.nextInt(mapRows);
+            int col = rand.nextInt(mapCols);
+            int randomFood = rand.nextInt(mapDataTypes.foodTypes.size());
+            Food food = mapDataTypes.foodTypes.get(randomFood);
+            if(tempMap[row][col] == null || tempMap[row][col].length() < 3) {
+                if(tempMap[row][col] == null) tempMap[row][col] = "E";
+                else tempMap[row][col] += "E";
+                pw.println("E," + food.getName() + "," + food.getDescription() + ", " +
+                        row + ", " + col + ", " + food.getWeight() + ", " + food.getSize() + ", "
+                        + food.getEnergy() + ",");
+                generatedCount++;
+            }
+        }
+    }
+    
+    private void generateFauna(PrintWriter pw, int numberOfFauna, String[][] tempMap) {
+        int generatedCount = 0;
+        Random rand = new Random();
+        while(generatedCount < numberOfFauna) {
+            int row = rand.nextInt(mapRows);
+            int col = rand.nextInt(mapCols);
+            int randomFauna = rand.nextInt(mapDataTypes.faunaTypes.size());
+            Fauna fauna = mapDataTypes.faunaTypes.get(randomFauna);
+            if(tempMap[row][col] == null || tempMap[row][col].length() < 3) {
+                if(tempMap[row][col] == null) tempMap[row][col] = "F";
+                else tempMap[row][col] += "F";
+                pw.println("F," + fauna.getName() + "," + fauna.getDescription() + ", " +
+                        row + ", " + col + ",");
+                generatedCount++;
+            }
+        }
+    }
+    
+    private void generateHazards(PrintWriter pw, int numberOfHazards, String[][] tempMap) {
+        int generatedCount = 0;
+        Random rand = new Random();
+        while(generatedCount < numberOfHazards) {
+            int row = rand.nextInt(mapRows);
+            int col = rand.nextInt(mapCols);
+            int randomHazard = rand.nextInt(mapDataTypes.hazardTypes.size());
+            Hazard hazard = mapDataTypes.hazardTypes.get(randomHazard);
+            //Dont spawn a predator within PRED_MIN_DIST spaces of a kiwi
+            boolean validSpawn = true;
+            for(int x = -HAZARD_MIN_DIST; x < HAZARD_MIN_DIST; x++) {
+                for(int y = -HAZARD_MIN_DIST; y < HAZARD_MIN_DIST; y++) {
+                    if(col+x > 0 && col+x < mapCols && row+y > 0 && row+y < mapRows) {
+                        if(tempMap[row+y][col+x] != null && tempMap[row+y][col+x].contains("H")) {
+                            validSpawn = false;
+                        }
+                    }
+                }
+            }
+            if(tempMap[row][col] != null && row == playerRow && col == playerCol) validSpawn = false;
+            if(validSpawn && (tempMap[row][col] == null || tempMap[row][col].length() < 3)) {
+                if(tempMap[row][col] == null) tempMap[row][col] = "H";
+                else tempMap[row][col] += "H";
+                pw.println("H," + hazard.getName() + "," + hazard.getDescription() + ", " +
+                        row + ", " + col + ", " + hazard.getImpact() + ",");
+                generatedCount++;
+            }
+        }
+    }
+    
+    private int generatePredators(PrintWriter pw, int numberOfPredators, String[][] tempMap) {
+        int generatedCount = 0;
+        int failedSpawns = 0;
+        Random rand = new Random();
+        while(generatedCount < numberOfPredators) {
+            int row = rand.nextInt(mapRows);
+            int col = rand.nextInt(mapCols);
+            int randomPredator = rand.nextInt(mapDataTypes.predatorTypes.size());
+            Predator predator = mapDataTypes.predatorTypes.get(randomPredator);
+            //Dont spawn a predator within PRED_MIN_DIST spaces of a kiwi
+            boolean validSpawn = true;
+            for(int x = -PRED_MIN_DIST; x < PRED_MIN_DIST; x++) {
+                for(int y = -PRED_MIN_DIST; y < PRED_MIN_DIST; y++) {
+                    if(col+x > 0 && col+x < mapCols && row+y > 0 && row+y < mapRows) {
+                        if(tempMap[row+y][col+x] != null && tempMap[row+y][col+x].contains("K")) {
+                            validSpawn = false;
+                        }
+                    }
+                }
+            }
+            if(tempMap[row][col] != null && tempMap[row][col].contains("H")) validSpawn = false;
+            if(validSpawn && (tempMap[row][col] == null || tempMap[row][col].length() < 3)) {
+                if(tempMap[row][col] == null) tempMap[row][col] = "P";
+                else tempMap[row][col] += "P";
+                pw.println("P," + predator.getName() + "," + predator.getDescription() + ", " +
+                        row + ", " + col + ",");
+                generatedCount++;
+                failedSpawns = 0;
+            } else {
+                failedSpawns++;
+            }
+            if(failedSpawns > 100) {
+                int failedGens = (numberOfPredators-generatedCount);
+                System.err.println("Unable to generate " + failedGens + " predators.");
+                return failedGens;
+            }
+        }
+        return 0;
+    }
+    
+    private void generateKiwis(PrintWriter pw, int numberOfKiwis, String[][] tempMap) {
+        int generatedCount = 0;
+        Random rand = new Random();
+        while(generatedCount < numberOfKiwis) {
+            int row = rand.nextInt(mapRows);
+            int col = rand.nextInt(mapCols);
+            int randomKiwi = rand.nextInt(mapDataTypes.kiwiTypes.size());
+            Kiwi kiwi = mapDataTypes.kiwiTypes.get(randomKiwi);
+            if(tempMap[row][col] == null || 
+                    (tempMap[row][col].length() < 3 && !tempMap[row][col].contains("H"))) {
+                if(tempMap[row][col] == null) tempMap[row][col] = "K";
+                else tempMap[row][col] += "K";
+                pw.println("K," + kiwi.getName() + "," + kiwi.getDescription() + ", " +
+                        row + ", " + col + ",");
+                generatedCount++;
+            }
+        }
+    }
+    
+    private void generateTools(PrintWriter pw, int[] numberOfTools, String[][] tempMap) {
+        for(int i = 0; i < numberOfTools.length; i++) {
+            int generatedCount = 0;
+            Random rand = new Random();
+            while(generatedCount < numberOfTools[i]) {
+                int row = rand.nextInt(mapRows);
+                int col = rand.nextInt(mapCols);
+                Tool tool = mapDataTypes.toolTypes.get(i);
+                if(tempMap[row][col] == null || tempMap[row][col].length() < 3) {
+                    if(tempMap[row][col] == null) tempMap[row][col] = "T";
+                    else tempMap[row][col] += "T";
+                    pw.println("T," + tool.getName() + "," + tool.getDescription() + ", " +
+                            row + ", " + col + ", " + tool.getWeight() + ", " + tool.getSize() + ",");
+                    generatedCount++;
+                }
+            }
+        }
     }
     
     public PrintWriter CreateFile() {
