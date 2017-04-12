dc96e6fbca225f5c988d84c6ce5464e2394543ed
Logan <Logan@Sean791022>
2017-04-13 10:35:10 +1200

    create SerializationTest Class

---
+++ test/nz/ac/aut/ense701/gameModel/SerializationTest.java
@@ -0,0 +1,22 @@
+package nz.ac.aut.ense701.gameModel;
+
+/**
+ * The test class SearilzationTest.
+ *
+ * @author SeanChang
+ */
+public class SerializationTest {
+
+    Game game;
+    Player player;
+    Position playerPosition;
+    Island island;
+
+    /**
+     * Default constructor for test class SerializationTest
+     */
+    SerializationTest() {
+
+    }
+
+}
