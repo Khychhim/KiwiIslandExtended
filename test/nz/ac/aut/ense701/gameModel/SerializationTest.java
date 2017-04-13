b183edfd98e5f49182a6a9ccd9c00855164fbe3c
Logan <Logan@Sean791022>
2017-04-14 10:11:22 +1200

    Adding test	-	A save file is created Adding test	-	File name is "data.dat"

--- test/nz/ac/aut/ense701/gameModel/SerializationTest.java
+++ test/nz/ac/aut/ense701/gameModel/SerializationTest.java
@@ -1,7 +1,11 @@
 package nz.ac.aut.ense701.gameModel;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -26,7 +30,7 @@ public class SerializationTest {
      * Test Game object is serializable
      */
     @Test
-    public void isSerializable() {
+    public void testGameIsSerializable() {
         boolean expResult = true;
         boolean isSerialbizable;
         try {
@@ -38,4 +42,72 @@ public class SerializationTest {
         }
         assertEquals(expResult, isSerialbizable);
     }
+
+    /*
+     * Test save file is created
+     */
+    @Test
+    public void testSaveFileIsCreated() {
+
+        boolean expResult = true;
+        boolean isFileCreated = false;
+        Game game = new Game();
+
+        //CREATE SAVE DATA
+        try {
+            if (game.getPlayer().isAlive()) {
+                FileOutputStream fstream = new FileOutputStream("data.dat");
+                ObjectOutputStream objSteram = new ObjectOutputStream(fstream);
+                objSteram.writeObject(game);
+                //CLOSE OBJECTSTREAM AND FILEOUTPUTSTREAM
+                objSteram.close();
+                fstream.close();
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(SerializationTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        //CHECK FILE EXISTS
+        File file = new File("data.dat");
+        if (file.exists() && !file.isDirectory()) {
+            isFileCreated = true;
+        }
+
+        assertEquals(expResult, isFileCreated);
+    }
+
+    /*
+     * Test save file named "data.dat"
+     */
+    @Test
+    public void testSaveFileIsName() {
+        boolean expResult = true;
+        boolean isFileNameSame = false;
+        String fileName = "data.dat";
+
+        Game game = new Game();
+        //CREATE SAVE DATA
+        try {
+            if (game.getPlayer().isAlive()) {
+                FileOutputStream fstream = new FileOutputStream("data.dat");
+                ObjectOutputStream objSteram = new ObjectOutputStream(fstream);
+                objSteram.writeObject(game);
+                //CLOSE OBJECTSTREAM AND FILEOUTPUTSTREAM
+                objSteram.close();
+                fstream.close();
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(SerializationTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        //CHECK FILE named "data.dat" exists
+        File file = new File(fileName);
+        if (file.exists() && !file.isDirectory()) {
+            isFileNameSame = true;
+        }
+
+        assertEquals(expResult, isFileNameSame);
+    }
+
+
 }
