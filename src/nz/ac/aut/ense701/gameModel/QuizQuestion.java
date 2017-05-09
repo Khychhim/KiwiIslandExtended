e81c444d19ed5947d766ba72bb103da9edbd63fe
Logan <Logan@Sean791022>
2017-05-10 10:08:30 +1200

    Create QuizQuestion Class which store question info

---
+++ src/nz/ac/aut/ense701/gameModel/QuizQuestion.java
@@ -0,0 +1,52 @@
+package nz.ac.aut.ense701.gameModel;
+
+/**
+ * This class that store question information
+ *
+ * @author Sean Chang
+ */
+public class QuizQuestion {
+
+    private int difficulty;
+    private String question;
+    private String questionOptions[];
+    private int correctOptionIndex;
+
+    //Score Value for different level of quiz
+    public static final int VALUE_LEVEL1_QUIZ = 1;
+    public static final int VALUE_LEVEL2_QUIZ = 2;
+    public static final int VALUE_LEVEL3_QUIZ = 3;
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public void setQuestionOptions(String[] options) {
+        this.questionOptions = options;
+    }
+
+    public void setCorrectOptionIndex(int correctOptionIndex) {
+        this.correctOptionIndex = correctOptionIndex;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    public String[] getQuestionOptions() {
+        return questionOptions;
+    }
+
+    public int getCorrectOptionIndex() {
+        return correctOptionIndex;
+    }
+
+}
