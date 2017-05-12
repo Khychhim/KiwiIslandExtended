1f1df313a05f3a746ab3d3769d5d6012e332eb51
sean791022 <sean791022@gmail.com>
2017-05-12 16:41:04 +1200

    Add button for load/save game. Ready for test

--- src/nz/ac/aut/ense701/gameModel/Deserialization.java
+++ src/nz/ac/aut/ense701/gameModel/Deserialization.java
@@ -1,27 +1,77 @@
 package nz.ac.aut.ense701.gameModel;
 
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Deserialization {
 
-    public Deserialization() {
+    private String FILE_NAME = "Save_Data.dat";
+    private Game game;
+
+    public static void main(String[] args) {
+        Deserialization loadObject = new Deserialization();
+
     }
 
-    public Game deserialize() {
+    public Deserialization() {
+        createAWindow();
+    }
+
+    /**
+     * This method create a window with a button of Save
+     */
+    public void createAWindow() {
+        JFrame frame = new JFrame("Load Game");
+
+        //create panel
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+
+        //create button
+        JButton button = new JButton("Load Game");
+        button.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent arg0) {
+                Load();
+            }
+        });
+
+        panel.add(button);
+
+        //setting for frame
+        frame.add(panel);
+        frame.setSize(300, 300);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
+
+    /**
+     * This method load game data(FILE_NAME)
+     *
+     * @return Game
+     */
+    public Game Load() {
         Game game = null;
         try {
             //get current directory location
-            String currentDirectory = System.getProperty("java.io.tmpdir");
+            String currentDirectory = System.getProperty("user.dir");
 
             //Import & load Game object
-            FileInputStream fstream = new FileInputStream("data.dat");
+            FileInputStream fstream = new FileInputStream(FILE_NAME);
             ObjectInputStream objectStream = new ObjectInputStream(fstream);
             game = (Game) objectStream.readObject();
 
             //Debug - print of loading path
-            System.out.println("Deserialize loaded from: " + currentDirectory + "data.dat");
+            System.out.println("Game loaded from: " + currentDirectory + "\\" + FILE_NAME);
 
             //Close FileStream and ObjectOutputStream
             objectStream.close();
--- src/nz/ac/aut/ense701/gameModel/Serialization.java
+++ src/nz/ac/aut/ense701/gameModel/Serialization.java
@@ -1,7 +1,13 @@
 package nz.ac.aut.ense701.gameModel;
 
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  * This is the class for save game purpose This class will save every
@@ -11,18 +17,64 @@ import java.io.ObjectOutputStream;
  */
 public class Serialization {
 
+    private String FILE_NAME = "Save_Data.dat";
+    private Game game;
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        Serialization saveObject = new Serialization(game);
+    }
+
     public Serialization(Game game) {
+        this.game = game;
+        createAWindow();
+    }
+
+    /**
+     * This method create a window with a button of Save
+     */
+    public void createAWindow() {
+        JFrame frame = new JFrame("Save Game");
+
+        //create panel
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+
+        //create button
+        JButton button = new JButton("Save Game");
+        button.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent arg0) {
+                Save();
+            }
+        });
+
+        panel.add(button);
+
+        //setting for frame
+        frame.add(panel);
+        frame.setSize(300, 300);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
+
+    /**
+     * This method execute save game data as file .dat
+     */
+    public void Save() {
         try {
             //get current directory location
-            String currentDirectory = System.getProperty("java.io.tmpdir");
+            String currentDirectory = System.getProperty("user.dir");
 
-                //export & saving Game object
-            FileOutputStream fostream = new FileOutputStream(currentDirectory + "data.dat");
+            //export & saving Game object
+            FileOutputStream fostream = new FileOutputStream(FILE_NAME);
             ObjectOutputStream iostream = new ObjectOutputStream(fostream);
             iostream.writeObject(game);
 
             //debug - output save successfully 
-            System.out.println("Serialzed data is saved: " + currentDirectory + "data.dat");
+            System.out.println("Data is saved: " + currentDirectory + "\\" + FILE_NAME);
 
             //close FileStream and ObjectOutputStream
             iostream.close();
@@ -30,7 +82,6 @@ public class Serialization {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 
 }
