7483b263c0be8988fbc53f9743799408a97e4e50
sean791022 <sean791022@gmail.com>
2017-05-12 15:54:18 +1200

    Add start predator timer when exist notification panel.(need to merge with trigger branch, where the function is written)

--- src/nz/ac/aut/ense701/gui/NotificationPanel.java
+++ src/nz/ac/aut/ense701/gui/NotificationPanel.java
@@ -98,8 +98,11 @@ public class NotificationPanel extends javax.swing.JPanel {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+        //Close the Notification Panel
         this.miniGamePanel.resultFrame.dispose();
+        //start predtor timer
+        //game.startPredatorTimer(); 
+        //!!!! This method is written in mini-game trigger Branch and its not merge so cant be use yet
     }//GEN-LAST:event_jButton1ActionPerformed
 
 
