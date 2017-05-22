b311aa659e36927cbdff053085af50177c719834
Nishan E. Jayetileke <jqj8595@autuni.ac.nz>
2017-05-22 21:34:53 +1200

    Created Facebook API class to authenticate Users.

---
+++ src/nz/ac/aut/ense701/gameModel/FacebookAPI.java
@@ -0,0 +1,27 @@
+package nz.ac.aut.ense701.gameModel;
+
+import com.restfb.DefaultFacebookClient;
+import com.restfb.FacebookClient;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Nishan
+ */
+public class FacebookAPI {
+    String accessToken ="EAACEdEose0cBALoULQD6MsDIgrngFrZCg7ZAVyyTUS5W5Y6ZBHcY8qsjFfFRt7oAuNgTldWp4RVGRPPyS9C8AnIEZBzXEZB6P3dSi3kBSHuwWziiCJ4FzWJdpMwJAE3T6eE2nJkRH73il5XZAXmC5R38WMW0khHZArQe3PLeoZCsibZBnXEXxIodk9xK2adRluF8ZD";
+    FacebookClient fbclient = new DefaultFacebookClient(accessToken);
+    
+    
+    
+    
+    public static void main(String args[]){
+   
+    }
+    
+}
