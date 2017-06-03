bd2cfbdd6d4282e2b840e664084d3e3157bf4636
Nishan E. Jayetileke <jqj8595@autuni.ac.nz>
2017-06-03 18:45:11 +1200

    Social Development Branch

--- src/nz/ac/aut/ense701/gameModel/TwitterAPI.java
+++ src/nz/ac/aut/ense701/gameModel/TwitterAPI.java
@@ -1,12 +1,10 @@
 package nz.ac.aut.ense701.gameModel;
-
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import twitter4j.Status;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
-
 import twitter4j.conf.ConfigurationBuilder;
 
 /*
